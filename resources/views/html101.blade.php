262765e2ea2219fbd917c83e130f9b2bff42d09b
Jaranaporn Pimwong <67160087@my.buu.ac.th>
2025-12-01 17:25:27 +0700

    test

---
+++ resources/views/html101.blade.php
@@ -0,0 +1,38 @@
+<!-- resources/views/html.blade.php -->
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Workshop HTML</title>
+        <link rel="stylesheet" href="css/bootstrap.css" />
+    </head>
+    <body>
+        <div class="container pt-5">
+            <h1> Workshop #HTML - FORM </h1>
+
+        </div>
+
+    <div class="container pt-6">
+  <div class="row align-items-start">
+    <div class="col-3">
+        <lable for="fname"class="col-form-control" >
+      ชื่อ
+</lable>
+    </div>
+    <div class="col-6">
+      <input type="text id="fname class="form-control">
+    </div>
+  </div>
+  <div class="row align-items-start">
+    <div class="col-3">
+      นามสกุล
+    </div>
+    <div class="col-6">
+      <input type="text id="fname class="form-control">
+    </div>
+  </div>
+</div>
+
+    </body>
+</html> 
